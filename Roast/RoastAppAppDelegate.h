92d253e9110e2ae2f43bad9e0864eaacfc181353
KT <KT@KTs-MacBook-Air.local>
2014-01-18 15:06:35 -0800

    @Kenny: Initial commit

---
+++ Roast/RoastAppAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RoastAppAppDelegate.h
+//  Roast
+//
+//  Created by KT on 1/17/14.
+//  Copyright (c) 2014 Affiliated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoastAppAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Roast/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Roast
+//
+//  Created by KT on 1/17/14.
+//  Copyright (c) 2014 Affiliated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RoastAppAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RoastAppAppDelegate class]));
+    }
+}
