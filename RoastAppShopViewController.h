fd7ba2e1412dcc0614b7e5721e8ab3027d56bd2c
Nick Variz <nicholas.variz@service-now.com>
2014-01-24 20:08:39 -0800

    @Nick: Relocated files based on there purpose, e.g. “Feed” or “ShopView. Created Shop ViewController custom class.

---
+++ RoastAppShopViewController.h
@@ -0,0 +1,13 @@
+//
+//  RoastAppShopViewController.h
+//  Roast
+//
+//  Created by Nicholas Variz on 1/24/14.
+//  Copyright (c) 2014 Affiliated. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RoastAppShopViewController : UIViewController
+
+@end
