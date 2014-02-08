3be7aab850e124e9b6c9ac37ba12ea1033ddb9a7
Nick Variz <nicholas.variz@service-now.com>
2014-02-08 00:03:30 -0800

    @Nick: Fixed the build (after I broke it after Kenny’s commit). My Bad.

--- RoastAppServerImageHandler.m
+++ RoastAppServerImageHandler.m
@@ -10,13 +10,10 @@
 
 @implementation RoastAppServerImageHandler
 
-//All server requests have this base URL
-NSString *baseURL = @"http://54.201.5.175:8080/roast/";
-
 // Get image from Tomcat server via <server ip>/roast/GetCafeImages?image=<filename>
 +(UIImage *)requestCafeImages:(NSString *)imageName{
     
-    NSString *fullURL = [[baseURL stringByAppendingString:@"GetCafeImages?image="] stringByAppendingString:imageName];
+    NSString *fullURL = [[@"http://54.201.5.175:8080/roast/" stringByAppendingString:@"GetCafeImages?image="] stringByAppendingString:imageName];
     
     UIImage* img = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:fullURL]]];
     
